2b8da7e75b04a4a73826a8acf202c06ad1bed097
likewise96 <andres.torres7@est.uexternado.edu.co>
2025-11-19 15:09:35 +0000

    cree funcion percentiles

--- funciones.py
+++ funciones.py
@@ -17,3 +17,27 @@ def desviacion(archivo, columna):
         print("Error al procesar la columna:", e)
         return None
     
+
+
+def percentiles(archivo, columna):
+    try:
+        df = pd.read_csv(archivo)
+        
+        if columna not in df.columns:
+            return "No existe la comuna"
+        
+        datos = df[columna]
+
+        
+        p25 = datos.quantile(0.25)
+        p50 = datos.quantile(0.50)
+        p75 = datos.quantile(0.75)
+        
+        return {
+            'percentil_25': round(float(p25), 2),
+            'percentil_50': round(float(p50), 2),
+            'percentil_75': round(float(p75), 2)
+        }
+        
+    except:
+        return "Error al procesar la columna"
