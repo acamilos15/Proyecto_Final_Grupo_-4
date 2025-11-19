058115a90edf6ebaf870b236d14b0fab295368c0
AndresBaquero359 <andres.baquero1@est.uexternado.edu.co>
2025-11-19 15:00:26 +0000

    agregue la función desviación en el archivo de funciones

--- funciones.py
+++ funciones.py
@@ -7,3 +7,13 @@ def promedios(archivo, columna):
     except Exception as e:
         print("Error al procesar la columna:", e)
         return None
+    
+
+def desviacion(archivo, columna):  
+    try: 
+        df = pd.read_csv(archivo)
+        return df['columna'].std()
+    except Exception as e:
+        print("Error al procesar la columna:", e)
+        return None
+    
